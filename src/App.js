422c7522318a4845959e2f04ad1b63bd480ea865
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-29 21:50:56 +0200

    making a todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Todo({ todo, index }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
+
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      text: 'learn React',
+      isCompleted: false
+    },
+    {
+      text: 'learn Javascript',
+      isCompleted: false
+    },
+    {
+      text: 'learn PHP',
+      isCompleted: false
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
