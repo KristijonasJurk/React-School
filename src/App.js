f7ad1d8196df725f665006d341adbd2759c6cda3
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-29 22:10:54 +0200

    added todo add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="todo">
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
       {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
     </div>
   )
 }
 
 
-function App() {
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className='input'
+        value={value}
+        placeholder="Add todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+function App() {
   const [todos, setTodos] = useState([
     {
       text: 'learn React',
@@ -27,12 +49,18 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
